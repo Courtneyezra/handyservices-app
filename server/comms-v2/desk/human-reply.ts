691c018196cb1b53233ba3073045180218bd9dbb
Courtnee <ezramarketingltd@gmail.com>
2026-09-17 12:27:02 +0000

    no-mistakes(test): Use "your enquiry" as reopen template subject without job type

--- server/comms-v2/desk/human-reply.ts
+++ server/comms-v2/desk/human-reply.ts
@@ -35,7 +35,6 @@ import { clauseAsks, offersCall } from './lexicon';
 import { isHeldAckText } from './fixed-lines';
 import { chooseChannel, DESK_APPROVER, liveTemplateStatus, pickTemplate, render, send, shortenBriefFor, windowOf, type TemplateStatusSource } from './sender';
 import { humanApprover, type Approver } from '../../approver';
-import { truncateWords } from '../channels/envelope';
 
 export interface HumanReplyInput {
     file: CaseFile;
@@ -257,7 +256,7 @@ export async function sendWindowTemplate(input: SendWindowTemplateInput, deps: C
     if (link) {
         pick = await pickTemplate('quote_ready', { name: party.name, topic: link, link, at: now() }, templates);
     } else if (heldOnQuestion(file) && unansweredQuestion(file, turn)) {
-        pick = await pickTemplate('service_reply', { name: party.name, topic: file.job.type ?? truncateWords(turn.body, 60), at: now() }, templates);
+        pick = await pickTemplate('service_reply', { name: party.name, topic: file.job.type ?? 'your enquiry', at: now() }, templates);
     } else {
         return refuse('no template is true for this thread: the customer needs to write again before a reply can go');
     }
